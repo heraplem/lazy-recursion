b2e6951e1b7830dc61e646517649f655c22182b5
Nicholas Coltharp <mail@heraplem.xyz>
2026-08-12 13:56:43 -0700

    Add comment explaining cofind

--- theories/Clairvoyance.v
+++ theories/Clairvoyance.v
@@ -101,6 +101,11 @@ CoInductive colist (A : Type) :=
 | cocons : A -> colist A -> colist A
 .
 
+(* cofindE is an effect representing a recursive call to the cofind function.
+   We introduce it locally, handle it using h_cofind, and then eliminate it
+   using cofind (via mrec).  See "Interaction Trees" for an explanation of this
+   approach; they demonstrate it using the Ackermann function. *)
+
 Inductive cofindE (A : Type) : Type -> Type :=
 | Cofind : nat -> colist A -> cofindE A bool
 .
