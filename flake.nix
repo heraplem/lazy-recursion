528fe3f3416d1669e4391ac00d8516e8d023e719
Nicholas Coltharp <mail@heraplem.xyz>
2026-08-13 16:01:29 -0700

    Update project
    - Add gitrees as a flake dependency.
    - Change expected Coq version to 8.20.

--- flake.nix
+++ flake.nix
@@ -2,18 +2,35 @@
   description = "Description for the project";
 
   inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    gitrees.url = "github:logsem/gitrees";
+    gitrees.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ self, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    # NOTE Add systems as tested.
     systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, ... }: {
+    perSystem = { system, inputs', pkgs, ... }: let
+      coqPkgsName = "coqPackages_8_20";
+      coqPkgs = pkgs.${coqPkgsName};
+    in {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            ${coqPkgsName} = prev.${coqPkgsName}.overrideScope (final: prev: {
+              gitrees = inputs'.gitrees.packages.coq-artifact;
+            });
+          })
+        ];
+      };
       devShells.default = pkgs.mkShell {
         packages = [
-          (pkgs.coq.withPackages (coqPkgs: [
-            coqPkgs.ITree
-          ]))
+          coqPkgs.coq
+          coqPkgs.ExtLib
+          coqPkgs.ITree
+          coqPkgs.gitrees
         ];
       };
     };
